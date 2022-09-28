fcc22b74abcd4c3534c859c02300f1d399030802
MeryAmun <chebesiawah@gmail.com>
2022-09-28 22:32:20 +0100

    creating users

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import dbRef,{db, userName, connectedRef} from './server/firebase';
+import { connect } from 'react-redux';
 
-const App = () => {
+
+const App = (props) => {
+    const participantsRef = dbRef.child("participants")
+    const getUserStream = async () => {
+        const localStream = await navigator.mediaDevices.getUserMedia({
+            audio:true,
+            video:true
+        });
+        return localStream;
+    };
+
+    useEffect(() => {
+        connectedRef.on("value", (snap) => {
+            if(snap.val()){
+                const defaultPreferences = {
+                    audio:true,
+                    video:false,
+                    screen:false
+                };
+              const userRef =   participantsRef.push({
+                    userName,
+                    preference:defaultPreferences
+
+                });
+                userRef.onDisconnect().remove()
+            }
+        })
+      
+    }, [])
+    
+
+    // useEffect( async () => {
+    //  const stream = await getUserStream();
+    //  stream.getVideoTracks()[0].enabled = false;
+    //  props.setMainStream(stream)
+    // }, [])
+    
   return (
-    <div>App</div>
+    <div>{userName}</div>
   )
 }
 
--- src/server/firebase.js
+++ src/server/firebase.js
@@ -1,15 +1,26 @@
 import firebase from 'firebase/app';
 import 'firebase/database'
 
+
 const firebaseConfig = {
+  apiKey: "AIzaSyAWUSlzuBPLm7NZ1D2xp6mOqxb9wW9-S5E",
+  authDomain: "meet-clone-5c6b0.firebaseapp.com",
+  databaseURL: "https://meet-clone-5c6b0-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "meet-clone-5c6b0",
+  storageBucket: "meet-clone-5c6b0.appspot.com",
+  messagingSenderId: "148405291780",
+  appId: "1:148405291780:web:1715014148b715272b1e51",
+  measurementId: "G-S417NSQZVR"
+};
 
-}
-
-
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-export const db = firebase;
+
+
+ export const db = firebase
 let dbRef = firebase.database().ref();
+export let connectedRef = firebase.database().ref(".info/connected")
 export const userName = prompt("What is your name")
 const urlParams = new URLSearchParams(window.location.search);
 const roomId = urlParams.get('id');
