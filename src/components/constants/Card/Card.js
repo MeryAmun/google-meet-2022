745a91225d874c21c96711ff14dac9019537f71a
MeryAmun <chebesiawah@gmail.com>
2022-09-29 16:06:53 +0100

    single participant component

--- src/components/constants/Card/Card.js
+++ src/components/constants/Card/Card.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './card.css'
 
-const Card = () => {
-  return (
-    <div>Card</div>
-  )
-}
+const Card = (props) => {
+  return <div className="card">{props.children}</div>;
+};
 
-export default Card
+export default Card;
--- src/components/participantScreen/ParticipantScreen.jsx
+++ src/components/participantScreen/ParticipantScreen.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
 import './participant.css'
+import Card from '../constants/Card/Card'
+import { faMicrophoneSlash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const ParticipantScreen = () => {
+const ParticipantScreen = (props) => {
+  const {
+    currentIndex, currentParticipant, hideVideo, videoRef,
+    showAvatar,currentUser 
+  } = props
   return (
-    <div>ParticipantsScreen</div>
-  )
-}
+    <div className={`participant ${hideVideo ? "hide" : ""}`}>
+<Card>
+  <video 
+  ref={videoRef}
+  className='video'
+  id={`participantVideo${currentIndex}`}
+  autoPlay
+  playsInline
+  >
+  </video>
+  {
+    !currentParticipant.audio && (
+      <FontAwesomeIcon
+      className='muted'
+      icon={faMicrophoneSlash}
+      title="Muted"
+      />
+    )}
+  {
+    showAvatar && (
+      <div className="avatar" 
+      style={{background: currentParticipant.avatarColor}}>
+{currentParticipant.name[0]}
+      </div>
+    )}
+    <div className="name">
+      {currentIndex.name}
+      {currentUser ? "(You)" : ""}
+    </div>
+</Card>
+    </div>
+  )}
 
 export default ParticipantScreen
